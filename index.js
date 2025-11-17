502f96f16099e6832df02c1b8a70b62ff7ad1a0a
nobirhoshensohag <nobirhoshensohag708@gmail.com>
2025-11-17 11:54:02 +0600

    add mongodb

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 const express = require("express");
 const cors = require("cors");
-
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 
 
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster1.h19g7bt.mongodb.net/?appName=Cluster1`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+async function run() {
+  try {
+    
+    await client.connect();
+    
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+    
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Smart server is running");
